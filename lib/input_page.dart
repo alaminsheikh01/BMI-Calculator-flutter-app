7091fd29c735ffac1ec8ea5d13773d5599c14b22
alamin <alamin@gmail.com>
2021-07-17 09:09:19 +0600

    use font awesome icon

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const activeCardColor = Color(0xFF1D1E33);
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,54 +12,152 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("BMI CALCULATOR"),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-                child: Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                    color: Color(0xFF1D1E33),
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                  cardChild: Column(
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'MALE',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Expanded(
-                  child: ReusableCard(
-                    color: Color(0xFF1D1E33),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                  cardChild: Column(
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'MALE',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      ),
+                    ],
                   ),
-                )
-              ],
-            )),
-            Expanded(
-              child: ReusableCard(
-                color: Color(0xFF1D1E33),
+                ),
+              )
+            ],
+          )),
+          Expanded(
+            child: ReusableCard(
+              color: activeCardColor,
+              cardChild: Column(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.mars,
+                    size: 80.0,
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    'MALE',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Color(0xFF8D8E98),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Expanded(
-                child: Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                    color: Color(0xFF1D1E33),
+          ),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                  cardChild: Column(
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'MALE',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Expanded(
-                  child: ReusableCard(
-                    color: Color(0xFF1D1E33),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  color: activeCardColor,
+                  cardChild: Column(
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'MALE',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      ),
+                    ],
                   ),
-                )
-              ],
-            )),
-          ],
-        ));
+                ),
+              )
+            ],
+          )),
+          Container(
+            color: Color(0xFFEB1555),
+            height: 80.0,
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 10.0),
+          )
+        ],
+      ),
+    );
   }
 }
 
 class ReusableCard extends StatelessWidget {
   final Color color;
-  ReusableCard({required this.color});
+  final Widget cardChild;
+  ReusableCard({required this.color, required this.cardChild});
 
   @override
   Widget build(BuildContext context) {
